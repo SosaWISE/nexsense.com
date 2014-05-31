c537d701ab23ede785e8e3f5ced8456ef6467f0a
Chris Heninger <chris@igravitydesign.com>
2014-05-31 12:28:48 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="favicon.png">
+
+	<title>nexsense</title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300|Montserrat:400' rel='stylesheet' type='text/css'>
+	<link href="css/style.css" rel="stylesheet">
+</head>
+<body class="homepage">
+	<header class="global-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-6">
+					<span class="logo"></span>
+				</div>
+				<div class="menu col-xs-6">
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="homepage-body">
+	</div>
+	<footer class="global-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-6">
+					Copyright 2014 &nbsp;|&nbsp; All Rights Reserved
+				</div>
+				<div class="col-xs-6 right">
+					Terms of Service &nbsp;|&nbsp; Privacy Policy
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
