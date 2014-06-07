9fafd94b8fb5b39116fdff6e071d8582a69f7319
Chris Heninger <chris@igravitydesign.com>
2014-06-07 02:10:03 -0700

    Re-worked the responsive layout for the bottom

--- index.php
+++ index.php
@@ -51,25 +51,27 @@
 			</div>
 		</div>
 		<div class="primary-section">
-			<div class="container">
+			<div class="container-fluid">
 				<div class="row">
 					<div class="built-to-fit col-md-6">
 						<div class="row">
-							<div class="inner col-md-10 col-md-offset-1">
+							<div class="inner col-xs-10 col-xs-offset-1">
 								<h2>Built to fit</h2>
 								<p>Body copy goes here lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-								<span><a href="#">See how</a></span>
+								<span>See how</span>
 							</div>
 						</div>
+						<a href="#"></a>
 					</div>
 					<div class="need-help col-md-6">
 						<div class="row">
-							<div class="inner col-md-10 col-md-offset-1">
+							<div class="inner col-xs-10 col-xs-offset-1">
 								<h2>Need help?</h2>
 								<p>Body copy goes here lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-								<span><a href="#">See how</a></span>
+								<span>See how</span>
 							</div>
 						</div>
+						<a href="#"></a>
 					</div>
 				</div>
 			</div>
@@ -88,15 +90,18 @@
 				<div class="row">
 					<div class="how-we-compare inner col-md-4">
 						<h2>How we compare</h2>
-						<span><a href="#">Learn more</a></span>
+						<span>Learn more</span>
+						<a href="#"></a>
 					</div>
 					<div class="how-we-help inner col-md-4">
 						<h2>How we help you save</h2>
-						<span><a href="#">See how</a></span>
+						<span>See how</span>
+						<a href="#"></a>
 					</div>
 					<div class="explore-our-system inner col-md-4">
 						<h2>Explore our system</h2>
-						<span><a href="#">Explore now</a></span>
+						<span>Explore now</span>
+						<a href="#"></a>
 					</div>
 				</div>
 			</div>
