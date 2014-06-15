443ea6974343f1dc882905113dfc973137ef76cd
Chris Heninger <chris.heninger@jobing.com>
2014-06-15 15:47:05 -0700

    Added nexsense-system page and a bunch of images

---
+++ nexsense-system.php
@@ -0,0 +1,73 @@
+<?php include('header.php');?>
+		<div class="hero-slider">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="copy col-md-6">
+						<div class="row">
+							<div class="copy-inner col-xs-10 col-xs-offset-1">
+								<p>Body copy goes here lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus euismod libero odio, sit amet fermentum risus tristique nec. Donec in venenatis neque, eu egestas felis.</p>
+							</div>
+						</div>
+					</div>
+					<div class="product-inner col-md-6">
+						<div class="row">
+							<div class="product-image col-xs-10 col-xs-offset-1">
+								<img src="assets/images/product-shot.png" width="100%">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="first-section">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="first-section-content col-md-5">
+						<div class="row">
+							<div class="inner col-md-10 col-md-offset-1">
+								<h2>Automative services</h2>
+								<p>Body copy goes here Facepudia vidus re mi, non corro eos delleni hicius volecta sus</p>
+								<span>Learn More</span>
+							</div>
+						</div>
+					</div>
+					<div class="first-section-image col-md-7">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="second-section">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="second-section-image col-md-6">
+					</div>
+					<div class="second-section-content col-md-6">
+						<div class="row">
+							<div class="inner col-md-10 col-md-offset-1">
+								<h2>Home, away from home</h2>
+								<p>Body copy goes here Facepudia vidus re mi, non corro eos delleni hicius volecta sus</p>
+								<span>Learn More</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="first-section">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="first-section-content col-md-5">
+						<div class="row">
+							<div class="inner col-md-10 col-md-offset-1">
+								<h2>Automative services</h2>
+								<p>Body copy goes here Facepudia vidus re mi, non corro eos delleni hicius volecta sus</p>
+								<span>Learn More</span>
+							</div>
+						</div>
+					</div>
+					<div class="first-section-image col-md-7">
+					</div>
+				</div>
+			</div>
+		</div>
+<?php include('footer.php');?>
