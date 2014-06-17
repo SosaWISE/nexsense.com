e42933db998691f57a351faf85b108f66b6c5123
Chris Heninger <chris.heninger@jobing.com>
2014-06-17 16:39:52 -0700

    Added a little more to the home away from home product page

--- home-away-from-home.php
+++ home-away-from-home.php
@@ -13,11 +13,20 @@
 				<div class="row">
 					<div class="panel-copy col-xs-12 col-sm-6">
 						<h2>Destia simin<br>cor sintem ut</h2>
-						<p style="width: 80%;">Nullorenimus di ommodignat fugiae conet quae magnatum et ut omnissit que voluptatem. Nem rero bea ped mi, ut qui as es verit ipsanda ndebis modignis iliquidis pro volo venit fugia volupid uciunt a dolorrovid quunt volorro eos explandiatet apist, quatur reheni temporerum ut et et aboreprate sunt.</p>
+						<p>Nullorenimus di ommodignat fugiae conet quae magnatum et ut omnissit que voluptatem. Nem rero bea ped mi, ut qui as es verit ipsanda ndebis modignis iliquidis pro volo venit fugia volupid uciunt a dolorrovid quunt volorro eos explandiatet apist, quatur reheni temporerum ut et et aboreprate sunt.</p>
 					</div>
 					<div class="panel-inner col-xs-12 col-sm-6">
 						<img src="assets/images/woman-on-phone.png" width="100%" />
 					</div>
+				</div>
+			</div>
+		</div>
+		<div class="home-shot"></div>
+		<div class="product-shot">
+			<div class="container">
+				<div class="row">
+					<img class="camera-product-shot" src="assets/images/camera-product-shot.png" />
+					ASDF
 				</div>
 			</div>
 		</div>
