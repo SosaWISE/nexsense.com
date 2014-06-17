8ed1189a2048a912137f807f4443cb6a1412e7b8
Chris Heninger <chris.heninger@jobing.com>
2014-06-17 16:52:17 -0700

    Added build your system page

--- navigation.php
+++ navigation.php
@@ -1,6 +1,6 @@
 <ul>
 	<li><a href="nexsense-system.php">Nexsense System</a></li>
-	<li><a href="#">Build Your System</a></li>
+	<li><a href="build-your-system.php">Build Your System</a></li>
 	<li><a href="customer-service.php">Customer Service</a></li>
 	<li><a href="about.php">About Us</a></li>
 	<li><a href="contact.php">Contact Us</a></li>
