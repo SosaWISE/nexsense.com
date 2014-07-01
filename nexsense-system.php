cef41c686150054f443a4f4850250dafdc4fd6b2
Chris Heninger <chris@igravitydesign.com>
2014-06-30 22:37:42 -0700

    nexsense-system page update

--- nexsense-system.php
+++ nexsense-system.php
@@ -1,73 +1,10 @@
 <?php include('header.php');?>
 	<div class="nexsense-system">
-		<div class="hero-slider">
-			<div class="container-fluid">
-				<div class="row-fluid">
-					<div class="one">
-						<div class="copy col-sm-6">
-							<div class="row">
-								<div class="copy-inner col-xs-10 col-xs-offset-1">
-									<h2>Safe doesn’t happen by accident</h2>
-									<p>Your home security system just got a lot smarter.</p>
-								</div>
-							</div>
-						</div>
-						<div class="product-inner col-sm-6 col-xs-hide">
-							<div class="row">
-								<div class="product-image col-xs-10 col-xs-offset-1">
-									<img src="assets/images/cta-product-panel.png" width="100%">
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="two" style="display: none;">
-						<div class="copy col-sm-6">
-							<div class="row">
-								<div class="copy-inner col-xs-10 col-xs-offset-1">
-									<h2>Don’t let distance come between you and your home</h2>
-									<p>Arm your home from your phone with Nexsense home automation.</p>
-								</div>
-							</div>
-						</div>
-						<div class="product-inner col-sm-6 col-xs-hide">
-							<div class="row">
-								<div class="product-image col-xs-10 col-xs-offset-1">
-									<img src="assets/images/cta-product-panel.png" width="100%">
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="three" style="display: none;">
-						<div class="copy col-sm-6">
-							<div class="row">
-								<div class="copy-inner col-xs-10 col-xs-offset-1">
-									<h2>like having eyes in the back of your head</h2>
-									<p>Get live, HD video instantly with Nexsense motion detecting cameras.</p>
-								</div>
-							</div>
-						</div>
-						<div class="product-inner col-sm-6 col-xs-hide">
-							<div class="row">
-								<div class="product-image col-xs-10 col-xs-offset-1">
-									<img src="assets/images/cta-product-camera.png" width="100%">
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="hero-controls">
-				<div class="dots">
-					<div class="dot active" rel="one"></div>
-					<div class="dot" rel="two"></div>
-					<div class="dot" rel="three"></div>
-				</div>
-			</div>
-		</div>
+		<?php include('hero-slider.php');?>
 		<div class="more-than-an-alarm clearfix">
 			<div class="container-fluid">
 				<div class="row-fluid">
-					<div class="inner col-xs-10 col-xs-offset-1 text-center">
+					<div class="inner col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 text-center">
 						<h1>More than an alarm.</h1>
 						<p>Security, yes.</p>
 						<p>But the Nexsense system is so much more. Control it from your phone. Monitor your house while you’re away. Arm it with the touch of a button from anywhere. We designed the Nexsense system to make things simple while keeping you safe. Here’s how.</p>
@@ -78,13 +15,13 @@
 		<div class="protects-your-family clearfix">
 			<div class="container-fluid">
 				<div class="row-fluid">
-					<div class="protects-your-family-image col-md-6"></div>
+					<div class="no-strings-attached-image col-md-6"></div>
 					<div class="corner-stripes col-md-6">
 						<div class="row">
 							<div class="inner col-md-10 col-md-offset-1">
-								<h2>Protects your family the same way you would.</h2>
-								<p>Home security should do more than just work. It should see what you can’t and hear what you don’t.</p>
-								<a class="btn btn-default" href="home-away-from-home.php">Learn More <img src="assets/images/caret-purple.png" /></a>
+								<h2>Live with no strings attached.</h2>
+								<p>You’re here. You’re there. You’re all over the place. With Nexsense, you get home security that fits the way you live—no matter where life takes you.</p>
+								<a class="btn btn-default" href="#">Here's How <img src="assets/images/caret-purple.png" /></a>
 							</div>
 						</div>
 					</div>
@@ -94,13 +31,13 @@
 		<div class="your-life-doesnt clearfix">
 			<div class="container-fluid">
 				<div class="row-fluid">
-					<div class="your-life-doesnt-image col-md-6 col-md-push-6"></div>
+					<div class="protects-your-family-image col-md-6 col-md-push-6"></div>
 					<div class="corner-stripes col-md-6 col-md-pull-6">
 						<div class="row">
 							<div class="inner col-md-10 col-md-offset-1">
-								<h2>Your life doesn’t stand still. So why should your home.</h2>
-								<p>Control your home from anywhere with your Nexsense system. No programming, no hassle—just safety and simplicity right from your phone. </p>
-								<a class="btn btn-default" href="home-away-from-home.php">Learn More <img src="assets/images/caret-purple.png" /></a>
+								<h2>Protects your family the same way you would.</h2>
+								<p>Home security should do more than just work. It should see what you can’t and hear what you don’t.</p>
+								<a class="btn btn-default" href="#">Learn More <img src="assets/images/caret-purple.png" /></a>
 							</div>
 						</div>
 					</div>
@@ -116,7 +53,7 @@
 							<div class="inner col-md-10 col-md-offset-1">
 								<h2>Now you’re ready for next.</h2>
 								<p>Great technology is at its best when it gives you more control and peace of mind for whatever life throws your way.</p>
-								<a class="btn btn-default" href="home-away-from-home.php">Learn More <img src="assets/images/caret-purple.png" /></a>
+								<a class="btn btn-default" href="#">Learn More <img src="assets/images/caret-purple.png" /></a>
 							</div>
 						</div>
 					</div>
