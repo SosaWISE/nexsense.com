631eaeb2aad10abb2ef5a84aba6e8fd3dafb81c8
Chris Heninger <chris@igravitydesign.com>
2014-06-30 22:49:17 -0700

    All content on choose-your-system page

--- choose-your-system.php
+++ choose-your-system.php
@@ -3,9 +3,8 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-12 text-center">
-						<h1>Keeping it simple</h1>
-						<span>Three great options</span>
-						<p>We have 3 great options that can protect your home well out of the box or be customize to fit your needs.<br>It’s all about our customer-first approach to home protection.</p>
+						<h1>explore your options</h1>
+						<p>There’s a right solution for every home.</p>
 					</div>
 				</div>
 			</div>
@@ -15,36 +14,96 @@
 				<div class="row">
 					<div class="col-xs-12 col-md-4">
 						<div class="options-intro">
-							<h2 class="text-center">Safe &amp; Sound</h2>
-							<span class="text-center">$49.99 a Month / $199 Activation</span>
-							<p>Have the ability to turn lights on and off, or shut off the TV from anywhere. You can monitor and watch over your home, truly making you king of your castle. Helping you and your family be Ready For Next.</p>
+							<h2>Total Control</h2>
+							<span class="price-block">$49.99 a Month<br>$199 Activation</span>
+							<p>Lights, cameras, heating, and more— control your entire home from anywhere with the touch of a button.</p>
 						</div>
-						<div class="included-equipment">
-							<h3 class="text-center">Included Equipment</h3>
-							<p>As eaquo blatem harum ipis volorer ciatus et exceperum nus qui sequi optaqui ni ipsunt volore nem vitem harchil labore, temolora porro everionem. Nem aspersp ediatent aute nonesed quam iuntor adit ipidele niminci mposam, occus idiae.</p>
+						<div class="what-you-get">
+							<p>In addition to protecting your home with wireless security, our Total Control package lets you build a complete home automation solution with all the bells and whistles you’ll ever need. Customize your home with HD cameras, remote door lock/unlock, lighting and temperature control, and so much more.</p>
+							<ul>
+								<li>2GIG Monitoring Panel</li>
+								<li>3 – Door Sensors</li>
+								<li>1 – Motion Detector</li>
+								<li>1 – Smoke &amp; Heat Detector</li>
+								<li>1 – Key Fob</li>
+								<li>Lifetime Warranty</li>
+								<li>24/7 Monitoring</li>
+								<li>Cellular Technology</li>
+								<li>MySense App *†</li>
+								<li>Video Camera *†</li>
+							</ul>
 						</div>
 					</div>
 					<div class="col-xs-12 col-md-4">
 						<div class="options-intro">
-							<h2 class="text-center">Basesense</h2>
-							<span class="text-center">$49.99 a Month / $69 Activation</span>
-							<p>Poriberit alis asimendae lam rat vel et et vidus dolore sequodis excest omniet es assusap icipsunda inctas corae eum ipsum hillaboria.</p>
+							<h2>Safe Home</h2>
+							<span class="price-block">$49.99 a Month<br>$69 Activation</span>
+							<p>Everything you need to keep your home safe and sound and the ability to control it all from your phone.</p>
 						</div>
-						<div class="included-equipment">
-							<h3 class="text-center">Included Equipment</h3>
-							<p>As eaquo blatem harum ipis volorer ciatus et exceperum nus qui sequi optaqui ni ipsunt volore nem vitem harchil labore, temolora porro everionem. Nem aspersp ediatent aute nonesed quam iuntor adit ipidele niminci mposam, occus idiae.</p>
+						<div class="what-you-get">
+							<p>Our basic package isn’t really that basic. It’s completely wireless, backed up by cellular technology, and it gives you instant, anywhere access to your system from your smart phone.</p>
+							<ul>
+								<li>2GIG Monitoring Panel</li>
+								<li>3 – Door Sensors</li>
+								<li>1 – Motion Detector</li>
+								<li>1 – Smoke &amp; Heat Detector</li>
+								<li>1 – Key Fob</li>
+								<li>Lifetime Warranty</li>
+								<li>24/7 Monitoring</li>
+								<li>Cellular Technology</li>
+								<li>MySense App *†</li>
+							</ul>
 						</div>
 					</div>
 					<div class="col-xs-12 col-md-4">
 						<div class="options-intro">
-							<h2 class="text-center">Up-to-speed</h2>
-							<span class="text-center">$39.99 a Month / $69 Activation</span>
-							<p>Have a current home protection system and need to get up-to-date. We can help you stay current by upgrading your old system to better protect you and your family. That’s how we roll.</p>
+							<h2>Hard-Wired+</h2>
+							<span class="price-block">$49.99 a Month<br>$69 Activation</span>
+							<p>We’ll update your existing hardwired system with a new panel and wireless components.</p>
 						</div>
-						<div class="included-equipment">
-							<h3 class="text-center">Included Equipment</h3>
-							<p>As eaquo blatem harum ipis volorer ciatus et exceperum nus qui sequi optaqui ni ipsunt volore nem vitem harchil labore, temolora porro everionem. Nem aspersp ediatent aute nonesed quam iuntor adit ipidele niminci mposam, occus idiae.</p>
+						<div class="what-you-get">
+							<p>While cellular security definitely has its benefits, sometimes that hardwired system that you have will do the trick. We’ll upgrade it with a Nexsense panel, wireless components, and a brand new lifetime warranty.</p>
+							<ul>
+								<li>2GIG Monitoring Panel</li>
+								<li>3 – Door Sensors</li>
+								<li>1 – Motion Detector</li>
+								<li>1 – Smoke &amp; Heat Detector</li>
+								<li>1 – Key Fob</li>
+								<li>Lifetime Warranty</li>
+								<li>24/7 Monitoring</li>
+							</ul>
 						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12">
+						<h2>Add-ons</h2>
+						<p>Want to add more or swap out one item for another on your home protection system, no problem.<br>We can help you customize your home protection system as needed.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-md-4">
+						<ul>
+							<li>MySense App *†</li>
+							<li>Video Camera *†</li>
+							<li>Automated Door Locks *†</li>
+						</ul>
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<ul>
+							<li>Small Appliance &amp; Lighting Control *†</li>
+							<li>Temperature Control *†</li>
+							<li>Medical Pendant *†</li>
+						</ul>
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<ul>
+							<li>* Require Cellular Technology</li>
+							<li>† Can be exchange or added to your home protection system.</li>
+						</ul>
+					</div>
+					<div class="col-xs-12">
+						<a class="btn btn-default" href="#">Get Started <img src="assets/images/caret-purple.png" /></a>
 					</div>
 				</div>
 			</div>
