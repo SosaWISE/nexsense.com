dc463a04c946272e2f43e8bdab70e654f4c570be
Chris Heninger <chris@2014-MacBook-Pro-41.local>
2014-07-17 13:27:41 -0600

    choose your system halfway done

--- choose-your-system.php
+++ choose-your-system.php
@@ -19,11 +19,11 @@
 				<div class="row">
 					<div class="col-xs-12 col-md-4">
 						<div class="options-intro">
-							<h2>Total Control</h2>
+							<h2><j class="visible-xs visible-sm hidden-md hidden-lg hidden-xl hidden-xxl"></j> Total Control</h2>
 							<span class="price-block" data-toggle="modal" data-target="#get-quote">$49.99 a Month<br>$199 Activation</span>
-							<p>Lights, cameras, heating, and more— control your entire home from anywhere with the touch of a button.</p>
+							<p class="hidden-xs hidden-sm visible-md visible-lg visible-xl visible-xxl">Lights, cameras, heating, and more— control your entire home from anywhere with the touch of a button.</p>
 						</div>
-						<span class="what-you-get-toggle"></span>
+						<span class="what-you-get-toggle hidden-xs hidden-sm visible-md visible-lg visible-xl visible-xxl"></span>
 						<div class="what-you-get slide" style="display: none;">
 							<p>In addition to protecting your home with wireless security, our Total Control package lets you build a complete home automation solution with all the bells and whistles you’ll ever need. Customize your home with HD cameras, remote door lock/unlock, lighting and temperature control, and so much more.</p>
 							<ul>
@@ -42,11 +42,11 @@
 					</div>
 					<div class="col-xs-12 col-md-4">
 						<div class="options-intro">
-							<h2>Safe Home</h2>
+							<h2><j class="visible-xs visible-sm hidden-md hidden-lg hidden-xl hidden-xxl"></j> Safe Home</h2>
 							<span class="price-block" data-toggle="modal" data-target="#get-quote">$49.99 a Month<br>$69 Activation</span>
-							<p>Everything you need to keep your home safe and sound and the ability to control it all from your phone.</p>
+							<p class="hidden-xs hidden-sm visible-md visible-lg visible-xl visible-xxl">Everything you need to keep your home safe and sound and the ability to control it all from your phone.</p>
 						</div>
-						<span class="what-you-get-toggle"><j></j> what you get</span>
+						<span class="what-you-get-toggle hidden-xs hidden-sm visible-md visible-lg visible-xl visible-xxl"><j></j> what you get</span>
 						<div class="what-you-get slide" style="display: none;">
 							<p>Our basic package isn’t really that basic. It’s completely wireless, backed up by cellular technology, and it gives you instant, anywhere access to your system from your smart phone.</p>
 							<ul>
@@ -64,11 +64,11 @@
 					</div>
 					<div class="col-xs-12 col-md-4">
 						<div class="options-intro">
-							<h2>Hard-Wired +</h2>
+							<h2><j class="visible-xs visible-sm hidden-md hidden-lg hidden-xl hidden-xxl"></j> Hard-Wired +</h2>
 							<span class="price-block" data-toggle="modal" data-target="#get-quote">$39.99 a Month<br>$69 Activation</span>
-							<p>We’ll update your existing hardwired system with a new panel and wireless components.</p>
+							<p class="hidden-xs hidden-sm visible-md visible-lg visible-xl visible-xxl">We’ll update your existing hardwired system with a new panel and wireless components.</p>
 						</div>
-						<span class="what-you-get-toggle"></span>
+						<span class="what-you-get-toggle hidden-xs hidden-sm visible-md visible-lg visible-xl visible-xxl"></span>
 						<div class="what-you-get slide" style="display: none;">
 							<p>While cellular security definitely has its benefits, sometimes that hardwired system that you have will do the trick. We’ll upgrade it with a Nexsense panel, wireless components, and a brand new lifetime warranty.</p>
 							<ul>
@@ -84,15 +84,15 @@
 					</div>
 				</div>
 			</div>
-			<div class="container-fluid add-ons slide" style="display: none;">
+			<div class="container-fluid add-ons slide visible-xs visible-sm hidden-md hidden-lg hidden-xl hidden-xxl">
 				<div class="row-fluid">
 					<div class="col-xs-10 col-xs-offset-1">
 						<h2>Endless possibilities</h2>
-						<p>With Nexsense, you can add, swap, or mix-and-match the technology you need to<br>build a home protection system that fits your home.</p>
+						<p>With Nexsense, you can add, swap, or mix-and-match the technology you need to build a home protection system that fits your home.</p>
 					</div>
 				</div>
 			</div>
-			<div class="container slide" style="display: none;">
+			<div class="container slide visible-xs visible-sm hidden-md hidden-lg hidden-xl hidden-xxl">
 				<div class="row">
 					<div class="col-xs-12 col-md-4">
 						<ul>
@@ -116,7 +116,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="container slide" style="display: none;">
+			<div class="container slide visible-xs visible-sm hidden-md hidden-lg hidden-xl hidden-xxl">
 				<div class="row">
 					<div class="get-started col-xs-12">
 						<a class="btn btn-default" data-toggle="modal" data-target="#get-quote">Get A Free Quote <i></i></a>
@@ -131,9 +131,10 @@
 			$('.what-you-get-toggle').on('click', function(e) {
 				$('j').toggleClass('active');
 				$('.slide').slideToggle();
-				$('html, body').animate({
-					scrollTop: $(this).offset().top
-				}, 1000);
+			});
+			$('.options-intro h2').on('click', function(e) {
+				$(this).find('j').toggleClass('active');
+				$(this).parent().siblings('.slide').slideToggle();
 			});
 		});
 	</script>
