282d51ba44a79512debd299c378d3a870972c960
Chris Heninger <chris@igravitydesign.com>
2014-07-30 22:22:35 -0700

    about page

--- about.php
+++ about.php
@@ -2,36 +2,36 @@
 	<div class="hero-mini"></div>
 	<div class="about-us">
 		<div class="container">
-			<div class="row">
+			<div class="row intro">
 				<div class="col-xs-12">
 					<h2>Who we are.</h2>
 					<p>We’re a home security and home automation provider who believes that a smarter, safer home should be simple to use and provide our customers with freedom as well as peace of mind. Driven by powerful technology and superior customer service, our goal is to create safer homes and families with affordable solutions and a proactive approach to security.</p>
 				</div>
 			</div>
-			<div class="row">
+			<div class="row intro">
 				<div class="col-xs-12">
 					<h2>What we do.</h2>
 					<p>Security, HVAC, lighting, and video—the solutions we provide are designed to streamline the way you interact with your home. Combined with a lifetime warranty, free moves, and affordable monthly billing, we’re building peace of mind and simplicity that saves you time and money.</p>
 				</div>
 			</div>
-			<div class="row">
+			<div class="row intro">
 				<div class="col-xs-12">
 					<h2>How we do it.</h2>
 					<p>Our commitment to an exceptional customer experience goes beyond providing great equipment and responsive service. That’s just the tip of the iceberg. Our approach to building safer homes comes down to three simple ideas. </p>
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-xs-12 col-md-4">
+				<div class="col-xs-12 col-md-4 text-center">
 					<span class="icon prepare"></span>
 					<h3>We help you prepare.</h3>
 					<p>We believe safety evolves. As technology advances and your needs change, we make sure you have the right solutions for your home and family.</p>
 				</div>
-				<div class="col-xs-12 col-md-4">
+				<div class="col-xs-12 col-md-4 text-center">
 					<span class="icon prepare"></span>
 					<h3>We make sure you’re aware.</h3>
 					<p>We want to connect you to your home in ways that make life simpler. Whether you’re 3,000 miles away or just down the hall, the more aware you are and the more responsive your home is, the safer you’ll feel.</p>
 				</div>
-				<div class="col-xs-12 col-md-4">
+				<div class="col-xs-12 col-md-4 text-center">
 					<span class="icon prepare"></span>
 					<h3>We put you in the know.</h3>
 					<p>From local alerts to national trends, we believe the more our customers know, the better. Facebook, email, Twitter, or a good old-fashioned phone call, when it comes to new risks as well as new opportunities, you’ll hear it from us first.</p>
