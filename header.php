cd5f5b0bc00892cc07592465915117328e0b23bb
jjenne <jjenne@nexsense.com>
2014-10-15 12:12:22 -0600

    added keywords and desc to meta

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Nexsense offers safety solutions that match your home and your lifestyle." />
+	<meta name="keywords" content="Nexsense, home security, alarm system, home automation, home management" />
 	<link rel="shortcut icon" href="//www.nexsense.com/favicon.ico">
 
 	<title><?php echo $title; ?></title>
