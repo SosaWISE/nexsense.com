e7fe3ce2ebbc2053a698f452b0921101abe239fd
jjenne <jjenne@nexsense.com>
2014-10-17 15:20:35 -0600

    attachment fix

--- upload-resume.php
+++ upload-resume.php
@@ -57,7 +57,7 @@ try {
 		if (count($matches) > 1) {
 			$contentType = $matches[1];
 			$extension = substr($contentType, strpos($contentType, '/')+1);
-			$attachment = chunk_split($request->resume);
+			$attachment = chunk_split(substr($request->resume, strpos($request->resume, 'base64,')+7));
 			$filename = $request->firstName . $request->lastName . '-resume.' . $extension;
 
 			$body .= "--$boundary\r\n";
