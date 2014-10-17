eeea251fcfb776bc8106d2312b3eef8b4279ab01
jjenne <jjenne@nexsense.com>
2014-10-17 09:25:49 -0600

    changes to resume upload

--- upload-resume.php
+++ upload-resume.php
@@ -3,85 +3,51 @@
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 
-$boundary = md5(date('r', time()));
+$boundary = 'NXS-'.md5(date('r', time()));
 
-$headers .= "Organization: Nexsense" . "\r\n";
-$headers .= "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type: text/plain; charset=iso-8859-1" . "\r\n";
-//$headers .= "Content-type: multipart/mixed; boundary=\"NXS-$boundary\"";
-$headers .= "From: no-reply@nexsense.com" . "\r\n";
-$headers .= "Reply-To: no-reply@nexsense.com" . "\r\n";
+$headers .= "Organization: Nexsense" . "\r\r\n";
+$headers .= "MIME-Version: 1.0" . "\r\r\n";
+//$headers .= "Content-type: text/plain; charset=iso-8859-1" . "\r\r\n";
+$headers .= "Content-type: multipart/mixed; boundary=\"$boundary\"";
+$headers .= "From: no-reply@nexsense.com" . "\r\r\n";
+$headers .= "Reply-To: no-reply@nexsense.com" . "\r\r\n";
 
 $emailto = 'jjenne@nexsense.com';
 $subject = "Sales Rep Application from Nexsense.com";
 
 
-$body = "NXS-$boundary\n";
-$body .= "Content-Type: multipart/alternative; boundary=\"$boundary\"\n\n";
+$body = '';
 
 // plain text section
-$body .= "$boundary\n";
-$body .= "Content-Type: text/plain; charset=\"iso-8859-1\"\n";
-$body .= "Content-Transfer-Encoding: 7bit\n\n";
+$body .= "$boundary\r\n";
+$body .= "Content-Type: text/plain; charset=\"iso-8859-1\"\r\n";
+$body .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
 
-$body = "Hey Doug,\n\n";
-$body .= "You've received another application from a sales rep candidate on Nexsense.com.\n\n";
-$body .= "Name: " . $request->firstName . " " . $request->lastName . "\n";
-$body .= "Email: " . $request->email . "\n";
-$body .= "Phone: " . $request->phone . "\n";
-$body .= "Zip Code: " . $request->zip . "\n";
-$body .= "\n";
+$body .= "Hey Doug,\r\n\r\n";
+$body .= "You've received another application from a sales rep candidate on Nexsense.com.\r\n\r\n";
+$body .= "Name: " . $request->firstName . " " . $request->lastName . "\r\n";
+$body .= "Email: " . $request->email . "\r\n";
+$body .= "Phone: " . $request->phone . "\r\n";
+$body .= "Zip Code: " . $request->zip . "\r\n";
+$body .= "\r\n";
 
 if (!empty($request->workHistory)) {
-	$body .= "Previous home security experience:\n";
+	$body .= "Previous home security experience:\r\n";
 	for ($i=0; $i<count($request->workHistory); $i++) {
-		$body .= "  Worked for " . $request->workHistory[$i]->company . " for " . $request->workHistory[$i]->yearsWorked . " years and sold " . $request->workHistory[$i]->annualSales . " accounts each year\n";
+		$body .= "  Worked for " . $request->workHistory[$i]->company . " for " . $request->workHistory[$i]->yearsWorked . " years and sold " . $request->workHistory[$i]->annualSales . " accounts each year\r\n";
 	}
-	$body .= "\n";
+	$body .= "\r\n";
 }
 else if (!empty($request->salesExperience)) {
-	$body .= "This candidate has previous home security sales experience, but has listed some other experience as follows:\n";
-	$body .= $request->salesExperience . "\n\n";
+	$body .= "This candidate has previous home security sales experience, but has listed some other experience as follows:\r\n";
+	$body .= $request->salesExperience . "\r\n\r\n";
 }
 
 if (!empty($request->sellYourself)) {
-	$body .= "Here's the candidate's 140-character pitch:\n";
-	$body .= $request->sellYourself;
+	$body .= "Here's the candidate's 140-character pitch:\r\n";
+	$body .= $request->sellYourself . "\r\n\r\n";
 }
 
-
-$body .= "\n\n$boundary\n";
-$body .= "Content-Type: text/html; charset=\"iso-8859-1\"\n";
-$body .= "Content-Transfer-Encoding: 7bit\n\n";
-
-// HTML version
-$body = "Hey Doug,\n\n";
-$body .= "You've received another application from a sales rep candidate on Nexsense.com.\n\n";
-$body .= "Name: " . $request->firstName . " " . $request->lastName . "\n";
-$body .= "Email: " . $request->email . "\n";
-$body .= "Phone: " . $request->phone . "\n";
-$body .= "Zip Code: " . $request->zip . "\n";
-$body .= "\n";
-
-if (!empty($request->workHistory)) {
-	$body .= "Previous home security experience:\n";
-	for ($i=0; $i<count($request->workHistory); $i++) {
-		$body .= "  Worked for " . $request->workHistory[$i]->company . " for " . $request->workHistory[$i]->yearsWorked . " years and sold " . $request->workHistory[$i]->annualSales . " accounts each year\n";
-	}
-	$body .= "\n";
-}
-else if (!empty($request->salesExperience)) {
-	$body .= "This candidate has previous home security sales experience, but has listed some other experience as follows:\n";
-	$body .= $request->salesExperience . "\n\n";
-}
-
-if (!empty($request->sellYourself)) {
-	$body .= "Here's the candidate's 140-character pitch:\n";
-	$body .= $request->sellYourself;
-}
-
-
-$body .= "\n\n$boundary\n\n";
 
 // attachment
 if (!empty($request->resumeData)) {
@@ -91,16 +57,18 @@ if (!empty($request->resumeData)) {
 		$extension = substr($contentType, strpos($contentType, '/')+1);
 		$attachment = chunk_split(substr($request->resumeData, strpos($request->resumeData, ';')+1));
 
-		$body .= "NXS-$boundary\n";
-		$body .= "Content-Type: $contentType; name=\"" . $request->firstName . $request->lastName . '-resume.' . $extension . "\"\n";
-		$body .= "Content-Transfer-Encoding: base64\n\n";
-		$body .= "Content-Disposition: attachment\n\n";
+		$body .= "$boundary\r\n";
+		$body .= "Content-Type: $contentType; name=\"" . $request->firstName . $request->lastName . '-resume.' . $extension . "\"\r\n";
+		$body .= "Content-Description: " . $request->firstName . $request->lastName . '-resume.' . $extension . "\r\n";
+		$body .= "Content-Disposition: attachment; filename=\"" .  . "\"\r\n";
+		$body .= "Content-Transfer-Encoding: base64\r\n\r\n";
 
 		$body .= $attachment;
+		$body .= "\r\n\r\n";
 	}
 }
 
-$body .= "\nNXS-$boundary\n\n";
+$body .= $boundary;
 
 
 
