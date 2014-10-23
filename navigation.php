468b4648c6134e35cc77d06877098988aae84498
jjenne <jjenne@nexsense.com>
2014-10-23 17:02:02 -0600

    mobile friendly careers page

--- navigation.php
+++ navigation.php
@@ -2,5 +2,7 @@
 <li rel="nexsense-system"><a href="nexsense-system">Nexsense System</a></li>
 <li rel="choose-your-system"><a href="choose-your-system">Choose Your System</a></li>
 <li rel="customer-service"><a href="customer-service">Customer Service</a></li>
+<li rel="blog"><a href="//blog.nexsense.com">Blog</a></li>
 <li rel="about-us"><a href="about">About Us</a></li>
 <li rel="contact-us"><a href="contact">Contact Us</a></li>
+<li rel="careers"><a href="careers">Careers</a></li>
